1ddcf493ace93bad42b031cdb0423f434410c422
steve <you@example.com>
2023-06-06 16:31:04 +0000

    fx

--- Visualisations/app.py
+++ Visualisations/app.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     functions={i:get_loss_fn(i,norm=True) for i in range(1,17)}
 
     app = Flask(__name__,template_folder='.')
-    app.config["SERVER_NAME"] = "scc-lanfotech.lancs.ac.uk"
+    app.config["SERVER_NAME"] = "scc-lanfotech.lancs.ac.uk:80"
     #app.config["SERVER_NAME"] = "localhost:5000"
     @app.route("/") 
     def index():
