ca8800dadbbaedcce678bf68e118265c382c21d3
steve <you@example.com>
2024-02-01 22:10:34 +0000

    add loop

--- launch.py
+++ launch.py
@@ -234,10 +234,14 @@ if __name__ == '__main__':
     NumTrials=hyperparams.num_trials
     #BEDE has Env var containing hostname  #HOSTNAME=login2.bede.dur.ac.uk check we arent launching on this node
     if NumTrials==-1:
-        trial=hyperparams.generate_trials(1)[0]
-        print("Running trial: {}".format(trial))
-        wandbtrain(trial)
-
+        while True:
+            try:
+                trial=hyperparams.generate_trials(1)[0]
+                print("Running trial: {}".format(trial))
+                wandbtrain(trial)
+            except Exception as e:
+                print(e)
+                continue
     elif NumTrials ==0 and not str(os.getenv("HOSTNAME","localhost")).startswith("login"): #We'll do a trial run...
         #means we've been launched from a BEDE script, so use config given in args///
         wandbtrain(hyperparams)
